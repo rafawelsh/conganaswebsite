118a0ff32f2f97013d4cc5109b5d47a27398e297
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2019-09-03 21:42:50 -0700

    beginning of Con Ganas website with Vue cli

--- src/router.js
+++ src/router.js
@@ -21,6 +21,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+    },
+    {
+      path: "/lookbook",
+      name: "lookbook",
+      component: () =>
+        import( /* webpackChunkName: "about" */ "./views/lookbook.vue")
+    },
   ]
 });
---
+++ src/views/lookbook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="lookbook">
+    <h1>The pictures will go here </h1>
+  </div>
+</template>
